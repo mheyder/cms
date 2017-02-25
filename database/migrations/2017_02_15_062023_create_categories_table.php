28ad6ccb2a09c42270c38acf7d480befdaf60862
mheyder <meirza.heyder@gmail.com>
2017-02-25 16:45:55 +0700

    [#138105145] Create migration for new table Categories

---
+++ database/migrations/2017_02_15_062023_create_categories_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->integer('parent_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropForeign('categories_parent_id_foreign');
+        });
+
+        Schema::dropIfExists('categories');
+    }
+}
